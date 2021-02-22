86f215f5b6183bd6bcf9325c7adc47762c5eeb9d
Vaughan Chandler <vaughan.codes@gmail.com>
2021-02-22 17:39:50 -0400

    Updates comments in gitrelease.sh

--- roles/main/files/usr_local_bin/gitrelease.sh
+++ roles/main/files/usr_local_bin/gitrelease.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 # Pushes all the changes after finishing a release with gitflow.
+# By Vaughan Chandler
 
 git push origin --tags && git checkout develop && git push && git checkout master && git push
 exit $?
