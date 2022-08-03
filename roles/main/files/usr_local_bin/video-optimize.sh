631d55c1138400aaee0f9c6412aba162e35d32e7
Vaughan Chandler <vaughan.codes@gmail.com>
2022-08-03 00:25:32 -0400

    Updates video-optimize.sh to include all streams by default.

--- roles/main/files/usr_local_bin/video-optimize.sh
+++ roles/main/files/usr_local_bin/video-optimize.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ $# -lt 1 ]; then
-    echo "Re-encodes a video using h265 with a specific CRF (defaults to 28)."
+    echo "Re-encodes a video using h265 with a specific CRF (defaults to 28; 0 is lossless)."
     echo "Usage: `basename $0` <video> [crf]"
     echo "Or:    `basename $0` <video> <crf> ..."
     echo "Where '...' represents args passed directly to ffmpeg, like '-an' to remove the audio stream."
@@ -11,5 +11,11 @@ fi
 file=$1; shift
 crf=${1:-28}; shift
 
-echo ffmpeg -i \"$file\" -c:v libx265 -crf $crf $@ \"${file%.*}.opt-$crf.${file##*.}\"
-ffmpeg -i "$file" -c:v libx265 -crf $crf $@ "${file%.*}.opt-$crf.${file##*.}"
+# Include all streams, unless a -map CLI arg was specified.
+map=
+if [[ ! $@ =~ "-map" ]]; then
+    map="-map 0"
+fi
+
+echo ffmpeg -i \"$file\" -c:v libx265 -crf $crf $map $@ \"${file%.*}.opt-$crf.${file##*.}\"
+ffmpeg -i "$file" -c:v libx265 -crf $crf $map $@ "${file%.*}.opt-$crf.${file##*.}"
