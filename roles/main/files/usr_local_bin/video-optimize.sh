faff1e3fbe180dc89b1d1b4ecfb803d52cf3ac1a
Vaughan Chandler <vaughan.codes@gmail.com>
2023-03-26 23:20:24 -0400

    Updates video-optimize script to support multiple files.

--- roles/main/files/usr_local_bin/video-optimize.sh
+++ roles/main/files/usr_local_bin/video-optimize.sh
@@ -2,20 +2,40 @@
 
 if [ $# -lt 1 ]; then
     echo "Re-encodes a video using h265 with a specific CRF (defaults to 28; 0 is lossless)."
-    echo "Usage: `basename $0` <video> [crf]"
-    echo "Or:    `basename $0` <video> <crf> ..."
-    echo "Where '...' represents args passed directly to ffmpeg, like '-an' to remove the audio stream."
+    echo "Usage: `basename $0` <video...> [crf] [args...]"
+    echo "Where <crf> is 0 - 51, and <args> are passed directly to ffmpeg, like '-an' to remove the audio stream."
+    echo "-- can be used to force all following arguments to be passed to ffmpeg."
     exit
 fi
 
-file=$1; shift
-crf=${1:-28}; shift
+crf=28
+files=()
+for arg in "$@"; do
+    if [[ $arg =~ ^([1-4]?[0-9]|5[01])$ ]]; then
+        crf=$arg
+        shift
+        break
+    elif [ "$arg" == "--" ]; then
+        shift
+        break
+    elif [ "$arg" == "-*" ]; then
+        break
+    elif [ -f "$arg" ]; then
+        files+=("$arg")
+        shift
+    else
+        2>&1 echo "Unknown argument: $arg"
+        exit 1
+    fi
+done
 
-# Include all streams, unless a -map CLI arg was specified.
 map=
 if [[ ! $@ =~ "-map" ]]; then
     map="-map 0"
 fi
 
-echo ffmpeg -i \"$file\" -c:v libx265 -crf $crf $map $@ \"${file%.*}.opt-$crf.${file##*.}\"
-ffmpeg -i "$file" -c:v libx265 -crf $crf $map $@ "${file%.*}.opt-$crf.${file##*.}"
+for file in "${files[@]}"; do
+    output_file="${file%.*}.opt-$crf.${file##*.}"
+    echo ffmpeg -i \"$file\" -c:v libx265 -crf $crf $map $@ \"$output_file\"
+    ffmpeg -i "$file" -c:v libx265 -crf $crf $map $@ "$output_file"
+done
